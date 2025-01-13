c3086d99a4c81836fa52eba075774935134071bf
sumon007 <sumons23up@gmail.com>
2025-01-13 12:04:14 +0530

    upload new project

---
+++ index.js
@@ -0,0 +1,75 @@
+
+    let name='sumon';
+    let age='30';
+    let hobby='Kite flying';
+
+    //console.log(typeof name);
+
+    let userDetails = {
+        name:'sumon',
+        age:'30',
+        hobby:'Kite flying',
+    }
+
+    let friends = ['sumon','nanai','saheb'];
+
+
+    function getDataResult(call1,call2){
+        let sumofdata = call1+call2;
+        return sumofdata;
+    }
+    const data = getDataResult(5,2);
+    //console.log(data);
+
+
+    
+
+ ///////////////////////////////--DO--//////////////////////////////////////////   
+let index = 100;
+
+while(index <=5){
+    //console.log('Hi sumon ',index);
+    index++;
+}
+
+let service = 100;
+
+do {
+    //console.log('Hi sonia ',service);
+    service++;
+}while(service <=5);
+//////////////////////////////////--DO--/////////////////////////////////////////
+
+
+
+///////////////////////////////////////////////////////////////////////////
+
+const objs = {
+    name : 'Ariyan',
+    age : 20
+}
+for(let key in objs){
+    //console.log(key, objs[key]);
+}
+
+let numbers = [1,2,3,4];
+
+for(let index in numbers){
+    //console.log(index, numbers[index]);
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+
+//////////////35. Break and continue//////////////////////////////////////
+
+for(let index=1; index <=20; index++){
+    
+    console.log(index);
+    if(index==10){
+        break
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////
+    
