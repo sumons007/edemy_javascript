3c6236342943258753dd04a01dcd711227dc0eca
sumon007 <sumons23up@gmail.com>
2025-01-14 10:53:08 +0530

    update some code

--- index.js
+++ index.js
@@ -24,7 +24,7 @@
 
     
 
- ///////////////////////////////--DO--//////////////////////////////////////////   
+ ///////////////////////////////--DO while--//////////////////////////////////////////   
 let index = 100;
 
 while(index <=5){
@@ -38,7 +38,7 @@ do {
     //console.log('Hi sonia ',service);
     service++;
 }while(service <=5);
-//////////////////////////////////--DO--/////////////////////////////////////////
+//////////////////////////////////--DO while end--/////////////////////////////////////////
 
 
 
@@ -65,17 +65,61 @@ for(let index in numbers){
 
 for(let index=1; index <=20; index++){
     
-    console.log(index);
+    //console.log(index);
     if(index==10){
         break
     }
 }
 
+for(i=1;i<=30;i++){
+    if(i==9){
+        continue
+    }
+    //console.log(i);
+}
+
 ///////////////////35. Break and continue////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////////////////
+//////////////////36. Infinite loops////////////////////////////////////////
+
+//for(let index=1; ; index++){
+    
+    //console.log(index);
+    
+//}
+
+//////////////////36. Infinite loops/////////////////////////////////////////
+
+//////////////////37. Nested Loops/////////////////////////////////////////
+
+// for(let ariyan=1; ariyan<=10; ariyan++){
+//     for(let kazi=1; kazi<=3; kazi++){
+//         console.log(ariyan, kazi);
+//     }
+    
+// }
+
+//////////////////37. Nested Loops/////////////////////////////////////////
+
+////////////////38. String Basics/////////////////////
+
+// var country ='India';
+// console.log(country);
+// console.log(country.length);
+// console.log(country.charAt(1));
+
+////////////////38. String Basics/////////////////////
 
 
+//39. String Methods: charAt, toUpperCase, toLowerCase, includes, startsWith, endsWith//
 
-///////////////////////////////////////////////////////////////////////////////
+var country ='India is my country';
+// console.log(country);
+// console.log(country[1]);
+// console.log(country.charAt(1));
+
+console.log(country.toLowerCase());
+console.log(country.toUpperCase());
+
+//39. String Methods: charAt, toUpperCase, toLowerCase, includes, startsWith, endsWith//
     
